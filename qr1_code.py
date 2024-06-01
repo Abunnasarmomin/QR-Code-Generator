e1da18879954c1a821057f51a642fc5beee09a33
Abunnasar-Programmer <150774835+Abunnasarmomin@users.noreply.github.com>
2024-06-01 13:07:11 +0530

    Add files via upload
    Just Input the link of your website, App ETC.

---
+++ qr1_code.py
@@ -0,0 +1,9 @@
+import qrcode as qr
+
+
+img = qr.make("https://www.facebook.com/profile.php/?id=100002549797116&name=xhp_nt_fbliteprofiletetratiles_friend_tile")
+img.save("Facebook-Qrcode.png")
+
+
+
+
